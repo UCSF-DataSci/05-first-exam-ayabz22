4ffc2837dd2d4c3866fe7df431162b43d44e04d1
Aya Boubezari <181286778+ayabz22@users.noreply.github.com>
2024-10-21 23:11:46 +0000

    made the complement function

---
+++ bioinformatics_project/scripts/dna_operations.py
@@ -0,0 +1,13 @@
+import sys 
+
+#first one is to the complement A to T and C to G 
+def complement(sequence):
+    dictionary = {'A':'T', 'C':'G', 'T':'A', 'G':'C'} #this is our dictionary of our bases
+    complements = [] #our empty list 
+    for base in sequence:
+        uppercase_base = base.upper() #convert the base to uppercase
+        complement_base = dictionary[uppercase_base] #take the uppercase and find it in the dictionary
+        complements.append(complement_base) #add it to the complements list 
+    return ''.join(complements)
+
+
