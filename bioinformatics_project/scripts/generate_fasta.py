7f4f764780b7b207d62c062825c4f4b407c5e0fc
Aya Boubezari <181286778+ayabz22@users.noreply.github.com>
2024-10-21 22:15:37 +0000

    sliced the chain into 80

--- bioinformatics_project/scripts/generate_fasta.py
+++ bioinformatics_project/scripts/generate_fasta.py
@@ -8,3 +8,9 @@ def generate_random_dna(length):
     return ''.join(dna) #then add " " so that theres no space so instead of A C G T it would be ACGT
 
 
+def save_fasta(filename, chain):
+    with open(filename, 'w') as file:
+        for i in range(0, len(chain), 80):
+            file.write(chain[i:i+80] + '\n') #use file.write to the write to a file, since we need to do slicing use the [:] 
+
+
